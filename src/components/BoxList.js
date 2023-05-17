aa5d9fea164eff3f742b653c3c9f57c56ed1e34f
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-17 10:33:03 -0700

    BoxList component added that renders newboxform and box components

--- src/components/BoxList.js
+++ src/components/BoxList.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Box from './Box';
 import NewBoxForm from './NewBoxForm';
+// import { v4 as uuidv4 } from 'uuid';
 // Place your state that contains all of the boxes here. 
 // This component should render all of the Box components 
 // along with the NewBoxForm component
 
 function BoxList() {
+    const [boxes, setBoxes] = useState([]);
+
+    // removed box by using id
+    const removeBox = (evt) => {
+        const {id} = evt.target;
+        setBoxes( box => box.filter( box => box.id !== id))
+    }
+    
     return (
-        <div> 
-        <NewBoxForm />
-         </div>
+        <> 
+        <NewBoxForm formDataHandler={setBoxes}/>
+        <div className='Box-container'>
+        {
+            boxes.map( 
+                ({width, height, color, id}) => <Box key={id} width={width} height={height} color={color} removeBox={removeBox} id={id}/>
+                )
+        }
+        </div>
+         </>
     )
 }
 
