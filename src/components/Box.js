06344556b9f000b4077284e62f74bedda26c88e4
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-17 10:32:03 -0700

    Box component added received 5 props (width, height, color, id, removeBox function)

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 // this component should display a div with a background color, 
 // width and height based on the props passed to it.
-function Box(props) {
+function Box({width, height, color, removeBox, id}) {
     return(
-        <div>Box</div>
+        <>
+            <div 
+                style={{
+                backgroundColor: color,
+                width: `${width}px`,
+                height: `${height}px`
+                }}
+            >
+            </div>
+            <button id={id} onClick={removeBox} style={ { height: `${height}px` } }>X</button>
+        </>
     )
 }
 
