7bf2bab3e2760ed6ac12492671d875a776e245e1
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-17 19:24:05 -0700

    smoke and snapshot test added

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders App without crashing', () => {
   render(<App />);
+});
+
+it('matches snapshot', () => {
+  const app = render(<App />);
+  expect(app.asFragment()).toMatchSnapshot();
 });
